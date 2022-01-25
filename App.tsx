4460f115f415352e0873d9ce78e28424549043d3
Miguel <herreraalcantarmiguelangel@gmail.com>
2022-01-25 23:53:03 +0100

    first commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import { HolaMundoScreens } from './src/screens/HolaMundoScreens';
+import { ContadorScreen } from './src/screens/ContadorScreen';
+
+const App = () => {
+  return (
+    // <HolaMundoScreens />
+  <ContadorScreen />
+  )
+};
+
+export default App;
---
+++ src/screens/HolaMundoScreens.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export const HolaMundoScreens = () => {
+  return (
+  <View style={{
+    flex: 1,
+    // backgroundColor: 'red',
+    justifyContent: 'center'
+  }}>
+    <Text style={{
+      fontSize: 45,
+      textAlign: 'center'
+    }}>Hola Miguel</Text>
+
+    
+  </View>
+  )
+};
+
